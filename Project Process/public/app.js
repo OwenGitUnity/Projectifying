f75aebb659f99abaebdb996cf1e0030dbe88098b
OwenGitUnity <86410823+OwenGitUnity@users.noreply.github.com>
2021-09-26 14:10:24 +0700

    done for this repository

--- Project Process/public/app.js
+++ Project Process/public/app.js
@@ -1,26 +1,19 @@
-//interfaces
-// interface IsPerson {
-//     name: string;
-//     age: number
-//     speak(a: string): void;
-//     spend(a:number): number;
-// }
-// const me: IsPerson = {
-//     name: 'shaun',
-//     age: 30,
-//     speak (text: string): void {
-//         console.log(text);
-//     },
-//     spend(amount: number): number { 
-//         console.log('i spent', amount);
-//         return amount;
-//     }
-// };
-// const greetPerson = (person: IsPerson) => {
-//     console.log('hello', person.name);
-// }
-// greetPerson(me)
-// console.log(me);
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('i spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
@@ -59,39 +52,3 @@ form.addEventListener('submit', (e) => {
     console.log(doc);
     list.render(doc, type.value, 'end');
 });
-// }) ; 
-// class sentComponent {
-//    public sentBy: string;
-//    public explanation: string|number;
-//    public amount: number;
-//     constructor(c: string, a: string, d: number) {
-//         this.sentBy = c;
-//         this.explanation = a;
-//         this.amount = d;
-//     }
-//         content(){
-//             return `${this.sentBy} owes $${this.amount} for ${this.explanation}`;
-//         };
-// }
-//     const sentOne = new sentComponent('yoshi', 'is the ceo of the yoshi company', 970);
-// const sentTwo = new sentComponent('bowser', 'is the ceo of the bowser company', 1550);
-// let sent : sentComponent[] = [];
-// sent.push(sentOne);
-// sent.push(sentTwo);
-// sent.forEach(inv =>{
-//     console.log(inv.sentBy, inv.explanation, inv.amount, inv.content());
-// })
-// const form = document.querySelector(".new-item-form")as HTMLFormElement; 
-// const type = document.querySelector('#type')as HTMLSelectElement;
-// const tofrom = document.querySelector('#tofrom')as HTMLInputElement;
-// const details = document.querySelector('#details')as HTMLInputElement;
-// const amount = document.querySelector('#amount')as HTMLInputElement;
-// form.addEventListener('submit', (e: Event) => {
-//     e.preventDefault();
-//     console.log(
-//         type.value,
-//         tofrom.value,
-//         details.value,
-//         amount.valueAsNumber
-//     );
-// });        
--- Project Process/src/app.ts
+++ Project Process/src/app.ts
@@ -1,29 +1,29 @@
-//interfaces
-// interface IsPerson {
-//     name: string;
-//     age: number
-//     speak(a: string): void;
-//     spend(a:number): number;
-// }
+// interfaces
+interface IsPerson {
+    name: string;
+    age: number
+    speak(a: string): void;
+    spend(a:number): number;
+}
 
-// const me: IsPerson = {
-//     name: 'shaun',
-//     age: 30,
-//     speak (text: string): void {
-//         console.log(text);
-//     },
-//     spend(amount: number): number { 
-//         console.log('i spent', amount);
-//         return amount;
-//     }
+const me: IsPerson = {
+    name: 'shaun',
+    age: 30,
+    speak (text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number { 
+        console.log('i spent', amount);
+        return amount;
+    }
     
-// };
-// const greetPerson = (person: IsPerson) => {
-//     console.log('hello', person.name);
-// }
-// greetPerson(me)
+};
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name);
+}
+greetPerson(me)
 
-// console.log(me);
+console.log(me);
 
 
 import { Invoice } from './classes/invoice.js';
@@ -80,50 +80,20 @@ form.addEventListener('submit', (e: Event) => {
     
     list.render(doc, type.value, 'end');
 
-});
-    
-// }) ; 
-// class sentComponent {
-//    public sentBy: string;
-//    public explanation: string|number;
-//    public amount: number;
+ });
 
-//     constructor(c: string, a: string, d: number) {
-//         this.sentBy = c;
-//         this.explanation = a;
-//         this.amount = d;
-//     }
-//         content(){
-//             return `${this.sentBy} owes $${this.amount} for ${this.explanation}`;
-//         };
+ //tuples
 
-    
-// }
-//     const sentOne = new sentComponent('yoshi', 'is the ceo of the yoshi company', 970);
-// const sentTwo = new sentComponent('bowser', 'is the ceo of the bowser company', 1550);
+ let arr  = ['ryu', 25, true];
+ arr[0] = false;
+ arr[1] = 'yoshi';
+ arr = [30, false, 'yoshi'];
 
-// let sent : sentComponent[] = [];
-// sent.push(sentOne);
-// sent.push(sentTwo);
+ let tup: [string, number, boolean] = ['ryu', 25, true]
+tup[0] = 'ken';
+tup[1] = 30;
 
-// sent.forEach(inv =>{
-//     console.log(inv.sentBy, inv.explanation, inv.amount, inv.content());
-// })
 
-// const form = document.querySelector(".new-item-form")as HTMLFormElement; 
 
-// const type = document.querySelector('#type')as HTMLSelectElement;
-// const tofrom = document.querySelector('#tofrom')as HTMLInputElement;
-// const details = document.querySelector('#details')as HTMLInputElement;
-// const amount = document.querySelector('#amount')as HTMLInputElement;
 
-// form.addEventListener('submit', (e: Event) => {
-//     e.preventDefault();
-
-//     console.log(
-//         type.value,
-//         tofrom.value,
-//         details.value,
-//         amount.valueAsNumber
-//     );
-// });        
+ 
